ab3c0016cb6290f9ad5061a449f65c49c272e55f
Shunsei <khan.sufyan.cs@gmail.com>
2020-07-29 13:36:22 -0400

    Adds delete route to delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,4 +139,23 @@ router.post('/', [
 	}
 });
 
+// @route 	DELETE api/profile/
+// @desc 	Deletes profile, user, and posts.
+// @access 	Private
+router.delete('/', auth, async (req, res) => {
+	try {
+	// @TODO Remove user's posts
+	// Remove profile
+	await Profile.findOneAndRemove({ user: req.user.id });
+
+	// Remove user
+	await User.findOneAndRemove({ _id: req.user.id });
+
+	res.json({ msg: 'User deleted.' });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error.');
+	}
+});
+
 module.exports = router;

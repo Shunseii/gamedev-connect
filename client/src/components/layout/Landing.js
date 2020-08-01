4d7378c3e74ab0a02a280c34b46b383a98a47f73
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-01 19:28:38 -0400

    Creates Landing and Navbar layout components

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Landing = () => {
+	return (
+		<section className="landing">
+			<div>
+				<div>
+					<h1>Gamedev Connect</h1>
+					<p>Connect with other developers and designers who share a common passion!</p>
+					<div>
+						<a href="#">Sign up</a>
+						<a href="#">Login</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Landing;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react' 
+
+const Navbar = () => {
+	return (
+		<nav>
+			<h1>Gamedev Connect</h1>
+			<ul>
+				<li><a href="#">Devs</a></li>
+				<li><a href="#">Register</a></li>
+				<li><a href="#">Login</a></li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;

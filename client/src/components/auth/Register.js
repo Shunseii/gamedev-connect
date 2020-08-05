9d82492319d1652fc2bc027324c8725c09bb7947
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-04 20:18:05 -0400

    Adds frontend form for registration of a new user

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Register = () => {
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: ''
+	});
+
+	const { name, email, password, password2 } = formData;
+
+	const onChange = e => setFormData(
+		{ ...formData, [e.target.name]: e.target.value }
+	);
+	
+	const onSubmit = e => {
+		e.preventDefault();
+		if (password !== password2) {
+			alert('Passwords do not match.');
+		} else {
+			console.log(formData);
+		}
+	};
+
 	return (
-		<div>
-			Register
-		</div>
+		<Fragment>
+			<h1>Sign Up</h1>
+			<p>Create your account</p>
+
+			<form onSubmit={e => onSubmit(e)}>
+				<input 
+					type='text' 
+					placeholder='Name' 
+					name='name' 
+					value={name} 
+					onChange={e => onChange(e)}
+					required 
+				/>
+				<input 
+					type='email' 
+					placeholder='Email Address' 
+					name='email'
+					value={email}
+					onChange={e => onChange(e)}
+					required
+				/>
+				<input 
+					type='password' 
+					placeholder='Password' 
+					name='password' 
+					minLength='6'
+					value={password}
+					onChange={e => onChange(e)}
+					required
+				/>
+				<input 
+					type='password' 
+					placeholder='Re-enter Your Password' 
+					name='password2' 
+					minLength='6'
+					value={password2}
+					onChange={e => onChange(e)}
+					required 
+				/>
+				<input type='submit' value='Register' />
+			</form>
+		</Fragment>
 	);
 };
 

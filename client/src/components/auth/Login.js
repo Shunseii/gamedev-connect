2b6365977330fd0ad676926cdc3714baf8a0973c
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-05 21:34:39 -0400

    Creates Login page layout and adds functionality to login with axios

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import axios from 'axios';
 
 const Login = () => {
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: ''
+	});
+
+	const { email, password } = formData;
+
+	const onChange = e => setFormData(
+		{ ...formData, [e.target.name]: e.target.value }
+	);
+	
+	const onSubmit = async e => {
+		e.preventDefault();
+		console.log('Logging in...');
+
+		const userToLogin = {
+			email,
+			password
+		};
+
+		try {
+			const config = {
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			};
+
+			const body = JSON.stringify(userToLogin);
+			const res = await axios.post('api/auth', body, config);
+
+			console.log(res.data);
+		} catch (err) {
+			console.error(err.response.data);
+		}
+	};
+
 	return (
-		<div>
-			Login
-		</div>
+		<Fragment>
+			<h1>Login</h1>
+			<p>Login to an existing account</p>
+
+			<form onSubmit={e => onSubmit(e)}>
+				<input 
+					type='email' 
+					placeholder='Email Address' 
+					name='email'
+					value={email}
+					onChange={e => onChange(e)}
+					required
+				/>
+				<input 
+					type='password' 
+					placeholder='Password' 
+					name='password'
+					value={password}
+					onChange={e => onChange(e)}
+					required
+				/>
+				<input type='submit' value='Login' />
+			</form>
+		</Fragment>
 	);
 };
 

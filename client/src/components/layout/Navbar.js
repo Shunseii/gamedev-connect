ac85893e36f0228571776cfc6fd8c009b62c401d
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-06 23:07:02 -0400

    Modifies styling for Navbar component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,17 +5,17 @@ import '../../tailwind.output.css';
 
 const Navbar = () => {
 	return (
-		<nav 
-			className='flex justify-between 
-						px-8 py-8
-						bg-blue-200'
+		<nav className='flex justify-between 
+						px-8 py-5
+						border-solid border-b-2 border-gray-200
+						shadow-inner bg-gray-100'
 		>
-			<h1 className='flex'>
-				<Link to='/'>Gamedev Connect</Link>
+			<h1 className=''>
+				<Link className='text-xl font-bold' to='/'>Gamedev Connect</Link>
 			</h1>
-			<ul className='inline-flex'>
-				<li className='mr-2'><Link to="/">Devs</Link></li>
-				<li className='mr-2'><Link to="/register">Register</Link></li>
+			<ul className='inline-flex w-1/4 justify-around'>
+				<li className=''><Link to="/">Devs</Link></li>
+				<li className=''><Link to="/register">Register</Link></li>
 				<li className=''><Link to="/login">Login</Link></li>
 			</ul>
 		</nav>

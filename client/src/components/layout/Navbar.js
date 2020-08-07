432ecd47edb27f78242fa19f8f68b888a9636a65
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-06 21:34:27 -0400

    Adds styles to navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,16 +1,22 @@
 import React from 'react' 
 import { Link } from 'react-router-dom'
 
+import '../../tailwind.output.css';
+
 const Navbar = () => {
 	return (
-		<nav>
-			<h1>
+		<nav 
+			className='flex justify-between 
+						px-8 py-8
+						bg-blue-200'
+		>
+			<h1 className='flex'>
 				<Link to='/'>Gamedev Connect</Link>
 			</h1>
-			<ul>
-				<li><Link to="/">Devs</Link></li>
-				<li><Link to="/register">Register</Link></li>
-				<li><Link to="/login">Login</Link></li>
+			<ul className='inline-flex'>
+				<li className='mr-2'><Link to="/">Devs</Link></li>
+				<li className='mr-2'><Link to="/register">Register</Link></li>
+				<li className=''><Link to="/login">Login</Link></li>
 			</ul>
 		</nav>
 	);

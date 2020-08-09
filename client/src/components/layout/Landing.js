a46ea9a018af252eeb3108a8a4466dd837c12aa0
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-09 17:52:01 -0400

    Adds styling for login and signup links

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,13 +3,27 @@ import { Link } from 'react-router-dom'
 
 const Landing = () => {
 	return (
-		<section className='flex justify-center items-center self-center h-screen'>
-			<div className='flex flex-col items-center'>
-				<h1>Gamedev Connect</h1>
+		<section className='flex justify-center items-center h-screen'>
+			<div className='flex flex-col'>
+				<h1 className='flex self-center text-4xl font-bold'>Gamedev Connect</h1>
 				<p>Connect with other developers and designers who share a common passion!</p>
-				<div className='flex justify-around self-stretch'>
-					<Link to="/register">Sign up</Link>
-					<Link to="/login">Login</Link>
+				<div className='flex justify-around items-center mt-3'>
+					<Link 
+						className='bg-blue-500 text-white font-bold rounded 
+									px-4 py-2
+									transition duration-200 ease-in-out hover:bg-blue-400' 
+						to="/register"
+					>
+						Sign up
+					</Link>
+					<Link 
+						className='bg-blue-500 text-white font-bold rounded 
+									px-4 py-2
+									transition duration-200 ease-in-out hover:bg-blue-400' 
+						to="/login"
+					>
+						Login
+					</Link>
 				</div>
 			</div>
 		</section>

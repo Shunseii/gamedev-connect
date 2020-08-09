bd46b423d70f591250504ba909806e5f550d6980
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-08 22:11:30 -0400

    Adds styling to Landing and Navbar components

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,15 +3,13 @@ import { Link } from 'react-router-dom'
 
 const Landing = () => {
 	return (
-		<section className="landing">
-			<div>
-				<div>
-					<h1>Gamedev Connect</h1>
-					<p>Connect with other developers and designers who share a common passion!</p>
-					<div>
-						<Link to="/register">Sign up</Link>
-						<Link to="/login">Login</Link>
-					</div>
+		<section className='flex justify-center items-center self-center h-screen'>
+			<div className='flex flex-col items-center'>
+				<h1>Gamedev Connect</h1>
+				<p>Connect with other developers and designers who share a common passion!</p>
+				<div className='flex justify-around self-stretch'>
+					<Link to="/register">Sign up</Link>
+					<Link to="/login">Login</Link>
 				</div>
 			</div>
 		</section>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,18 +5,18 @@ import '../../tailwind.output.css';
 
 const Navbar = () => {
 	return (
-		<nav className='flex justify-between 
+		<nav className='fixed w-full flex justify-between 
 						px-8 py-5
 						border-solid border-b-2 border-gray-200
 						shadow-inner bg-gray-100'
 		>
-			<h1 className=''>
+			<h1>
 				<Link className='text-xl font-bold' to='/'>Gamedev Connect</Link>
 			</h1>
 			<ul className='inline-flex w-1/4 justify-around'>
-				<li className=''><Link to="/">Devs</Link></li>
-				<li className=''><Link to="/register">Register</Link></li>
-				<li className=''><Link to="/login">Login</Link></li>
+				<li className='hover:text-gray-700'><Link to="/">Devs</Link></li>
+				<li className='hover:text-gray-700'><Link to="/register">Register</Link></li>
+				<li className='hover:text-gray-700'><Link to="/login">Login</Link></li>
 			</ul>
 		</nav>
 	);

63783fc1151566222c48db24b5f3e5f12b7ccfa9
Shunsei <khan.sufyan.cs@gmail.com>
2020-09-03 07:08:20 -0400

    Adds action to create/update profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -25,3 +25,38 @@ export const getCurrentProfile = () => async dispatch => {
 		// dispatch(setAlert(err.response.data.msg, 'failure'));
 	}
 };
+
+// Create or update a profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+	try {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+
+		const res = await axios.post('/api/profile', formData, config);
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		});
+
+		dispatch(setAlert((edit ? 'Profile Updated' : 'Profile Created'), 'success'));
+
+		if (!edit) {
+			history.push('/dashboard');
+		}
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach(error => dispatch(setAlert(error.msg, 'failure')));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.data.msg, status: err.response.status }
+		});
+	}
+};

e5add3df425c679569a1f16eba265f7a1c5dfcfb
Shunsei <khan.sufyan.cs@gmail.com>
2020-09-03 08:07:46 -0400

    Fixes border-top-color of loading spinner not being displayed

--- client/src/components/layout/LoadingSpinner/LoadingSpinner.js
+++ client/src/components/layout/LoadingSpinner/LoadingSpinner.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import './LoadingSpinner.styles.css';
-
 const LoadingSpinner = () => {
 	return (
 		<div className='h-full w-full flex justify-center items-center'>
 			<div className='LoadingSpinner__Container h-8 w-8 
-							border-4 border-gray-200 rounded-full
+							border-4 border-gray-200 border-opacity-0.5 rounded-full
 							animate-spin'>
 			</div>
 		</div>
